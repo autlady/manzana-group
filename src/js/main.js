91a7d6ff8b87be264687647ffd5add7614e2ad54
Julia Grigoreva <julia_panina@mail.ru>
2025-02-16 19:27:38 +0300

    adaptive partner section

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,32 @@
-// const toggleMenu = document.querySelector('menu-toggle');
+$(document).ready(function() {
+  //partners-slider
+  $('.partners-slider').slick({
+    infinite: true,
+    draggable: false,
+    swipe: false,
+		speed: 5000, // Чем больше, тем медленнее движение
+		slidesToShow: 7,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 0, // Бесшовный эффект
+		cssEase: 'linear', // Линейное движение
+		pauseOnHover: false, // Не останавливать при наведении
+		pauseOnFocus: false,
+		variableWidth: true,
+});
 
-// if (toggleMenu){
-//     toggleMenu.addEventListener('click', function(){
-        
-//         if(this.classList.contains('active')){
-//             this.classList.remove('active');
-//         }else{
-//             this.classList.add('active');
-//         }
-//     })
-// }
+  function updateSlidesToShow() {
+    let slider = $('.partners-slider');
+    let sliderWidth = slider.width(); // Получаем ширину слайдера
+    let slideWidth = slider.find('.slick-slide').outerWidth(true); // Получаем ширину одного слайда
+    let slidesToShow = Math.floor(sliderWidth / slideWidth); // Рассчитываем количество
+    slider.slick('slickSetOption', 'slidesToShow', slidesToShow > 0 ? slidesToShow : 1, true);
+  }
 
-
-
-const swiper = new Swiper('#products-slider', {
-
-  slidesPerView: 3,
-  spaceBetween: 32,
-  loop: true,
-
-  pagination: {
-    el: '#products-slider-pagination',
-  },
-
-  navigation: {
-    nextEl: '#btn-next',
-    prevEl: '#btn-prev',
-  },
+  $(window).on('load resize', function() {
+    updateSlidesToShow();
+});
 });
 
 
