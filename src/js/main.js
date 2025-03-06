bc36e5e87ca2a4c625add03e2591856bca8c01b8
Julia Grigoreva <julia_panina@mail.ru>
2025-03-06 20:59:01 +0300

    add modal call form

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
   const burger = $('.menu-toggle');
   const bodyEl = document.body;
 
-//   function removeActive() {
-//     $(overlay).removeClass('active');
-//     $(burger).removeClass('active');
-//     $(bodyEl).removeClass('noscroll');
-// }
+  function removeActive() {
+    $(overlay).removeClass('active');
+    $(burger).removeClass('active');
+    $(bodyEl).removeClass('noscroll');
+}
 
   function resize() {
     const menu = $('header .header__inner .menu');
@@ -33,7 +33,7 @@ resize();
 
 $(window).resize(function() {
     resize();
-    // removeActive();
+    removeActive();
 });
 
 burger.click(function(){
@@ -60,9 +60,7 @@ $('.menu a').click(function(){
   var widthHead = header.width();
   if(widthHead < 1024)
       $('.menu').slideUp(400);
-      $(overlay).removeClass('active');
-      $(burger).removeClass('active');
-      $(bodyEl).removeClass('noscroll');
+      removeActive();
 });
 
 $('.menu .header__btn').click(function(){
@@ -70,10 +68,64 @@ $('.menu .header__btn').click(function(){
   var widthHead = header.width();
   if(widthHead < 1024)
       $('.menu').slideUp(400);
-      $(overlay).removeClass('active');
-      $(burger).removeClass('active');
-      $(bodyEl).removeClass('noscroll');
+      removeActive();
 });
+
+//MODAL
+
+const modalFramesOpen = document.querySelectorAll('[data-modal-button]');
+let modalFrames = document.querySelectorAll('[data-modal]');
+
+if (modalFrames.length > 0) {
+    const modalFramesClose = document.querySelectorAll('[data-modal-close]');
+
+    for (let item of modalFramesOpen) {
+        item.addEventListener('click', function (e) {
+            for (let item of modalFrames) {
+
+                item.classList.add('hidden');
+                bodyEl.classList.remove('noscroll');
+            }
+
+            e.preventDefault();
+
+            const itemAttr = item.getAttribute('data-modal-button');
+
+            for (let frame of modalFrames) {
+                const frameAttr = frame.getAttribute('data-modal');
+                if (frameAttr === itemAttr) {
+                    frame.classList.remove('hidden');
+                    bodyEl.classList.add('noscroll');
+                }
+            }
+
+        });
+    }
+    /*==  закрыть модалки  frame-modal по клику на кнопку ======*/
+    for (let item of modalFramesClose) {
+
+        item.addEventListener('click', function (e) {
+
+            e.preventDefault();
+            item.closest('[data-modal]').classList.add('hidden');
+            bodyEl.classList.remove('noscroll');
+        });
+    }
+    /*=============== закрыть модалки по клику вне ===============*/
+    for (let frame of modalFrames) {
+
+        const frameWin = frame.querySelector('.modal-window');
+        frameWin.addEventListener('click', function (e) {
+            if (e.target === e.currentTarget) {
+                e.preventDefault();
+                frame.classList.add('hidden');
+                bodyEl.classList.remove('noscroll');
+            }
+        });
+    }
+};
+
+
 
   //partners-slider
   $('.partners-slider').slick({
